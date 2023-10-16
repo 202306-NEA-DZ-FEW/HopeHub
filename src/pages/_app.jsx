4cfaca33e805b68c897485394e1b74680c035438
Brahim HM <brahimhm98@gmail.com>
2023-10-16 22:39:35 +0100

    feat(footer component): style a footer component
    #3

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,15 @@
 import { appWithTranslation } from "next-i18next";
+import Footer from "@/components/Footer";
 
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default appWithTranslation(MyApp);
