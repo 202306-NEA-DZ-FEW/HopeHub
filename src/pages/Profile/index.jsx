11b4ee1ad68b4b2d7108fde83e1e0d8277cf04dc
Brahim HM <brahimhm98@gmail.com>
2023-10-22 20:47:08 +0100

    feat(profile info): styling the profile info page
    #9

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,14 +1,15 @@
 import Layout from "@/layout/Layout";
 import React from "react";
-import { FaUser, FaUserEdit } from "react-icons/fa";
+import { useTranslation } from "next-i18next";
+import { FaUser, FaPlus } from "react-icons/fa";
 import { LiaUserEditSolid } from "react-icons/lia";
-
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function UserProfile() {
+    const { t } = useTranslation("common");
     return (
         <Layout>
-            <div className='flex justify-center mt-8 font-poppins font-black'>
+            <div className='flex justify-center mt-8 font-poppins font-black flex-col md:flex-row lg:flex-row xl:flex-row'>
                 <div className='w-1/3 p-12 pl-40'>
                     <div className=' bg-black w-32 h-32 rounded-full justify-center flex items-center'>
                         <FaUser className='fill-white text-6xl'></FaUser>
@@ -17,15 +18,15 @@ export default function UserProfile() {
                         <LiaUserEditSolid className=' text-xl'></LiaUserEditSolid>
                     </div>
                 </div>
-                <div class='flex items-center justify-center p-12 w-2/3'>
+                <div class='flex items-center justify-center md:p-12 lg:p-12 md:w-2/3 lg:w-2/3 px-10'>
                     <div class='mx-auto w-full max-w-[550px] bg-white'>
                         <h2 className=' text-[50px] not-italic leading-[normal]'>
-                            PROFILE INFO
+                            {t("profile info")}
                         </h2>
                         <form>
                             <div class='mb-5 flex'>
                                 <label for='name' class=' mt-4 w-1/2 '>
-                                    Full Name
+                                    {t("full name")}
                                 </label>
                                 <input
                                     type='text'
@@ -36,22 +37,26 @@ export default function UserProfile() {
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='name' class=' mt-4 w-1/2 '>
-                                    Education Level
+                                    {t("education level")}
                                 </label>
                                 <select
                                     className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6  outline-none '
                                     name='education'
                                 >
-                                    <option value='value1'>Secondary</option>
-                                    <option value='value2' selected>
-                                        Bachelor
+                                    <option value='value1'>
+                                        {t("secondary")}
                                     </option>
-                                    <option value='value3'>Master</option>
+                                    <option value='value2' selected>
+                                        {t("bachelor")}
+                                    </option>
+                                    <option value='value3'>
+                                        {t("master")}
+                                    </option>
                                 </select>
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='name' class=' mt-4 w-1/2 '>
-                                    Hobbies
+                                    {t("hobbies")}
                                 </label>
                                 <input
                                     type='text'
@@ -62,33 +67,40 @@ export default function UserProfile() {
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='Family Size' class=' mt-4 w-1/2 '>
-                                    Family Size
+                                    {t("family size")}
                                 </label>
-                                <input
-                                    type=' number'
-                                    name='Family Size'
-                                    id='Family Size'
-                                    class='rounded-md border border-[#e0e0e0] bg-white py-3 px-6 outline-none '
-                                />
-                                <span className='mt-4 '> Member(s)</span>
+                                <div className=' w-full inline-flex gap-4'>
+                                    <input
+                                        type='number'
+                                        name='Family Size'
+                                        id='Family Size'
+                                        min={1}
+                                        max={100}
+                                        class='rounded-md border border-[#e0e0e0] bg-white py-3 outline-none text-center '
+                                    />
+                                    <span className='mt-4 '>
+                                        {" "}
+                                        {t("members")}
+                                    </span>
+                                </div>
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='gender' class=' mt-4 w-1/2 '>
-                                    Gender
+                                    {t("gender")}
                                 </label>
                                 <select
                                     className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6  outline-none '
                                     name='gender'
                                 >
-                                    <option value='Male'>Male</option>
+                                    <option value='Male'>{t("male")}</option>
                                     <option value='Femmale' selected>
-                                        Femmale
+                                        {t("female")}
                                     </option>
                                 </select>
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='Birth Date' class=' mt-4 w-1/2 '>
-                                    Birth Date
+                                    {t("birth date")}
                                 </label>
                                 <input
                                     type='date'
@@ -98,7 +110,7 @@ export default function UserProfile() {
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='email' class=' mt-4 w-1/2 '>
-                                    Email
+                                    {t("email")}
                                 </label>
                                 <input
                                     type='email'
@@ -109,7 +121,7 @@ export default function UserProfile() {
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='phone' class=' mt-4 w-1/2 '>
-                                    Phone Number
+                                    {t("phone number")}
                                 </label>
                                 <input
                                     type='phone'
@@ -120,107 +132,93 @@ export default function UserProfile() {
                             </div>
                             <div class='mb-5 flex'>
                                 <label for='IDcard' class=' mt-4 w-1/2 '>
-                                    Upload ID
+                                    {t("upload id")}
+                                </label>
+                                <div className='overflow-hidden relative w-full '>
+                                    <button
+                                        disabled
+                                        class='w-full rounded-md border border-[rgb(224,224,224)]  px-6 inline-flex items-center'
+                                    >
+                                        <input
+                                            type='file'
+                                            accept='image/*,.pdf'
+                                            name='IDcard'
+                                            class='w-full rounded-md py-3 outline-none  cursor-pointer  opacity-0 '
+                                        />
+                                        <FaPlus></FaPlus>
+                                    </button>
+                                </div>
+                            </div>
+                            <label class='mb-5 block text-4xl font-semibold '>
+                                {t("security")}
+                            </label>
+                            <div class='mb-5 flex'>
+                                <label for='password' class=' mt-4 w-1/2 '>
+                                    {t("password")}
                                 </label>
                                 <input
-                                    type='file'
-                                    accept='image/*,.pdf'
-                                    name='IDcard'
+                                    type='password'
+                                    name='password'
+                                    id='password'
                                     class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 outline-none '
                                 />
                             </div>
-
-                            <div class='-mx-3 flex flex-wrap'>
-                                <div class='w-full px-3 sm:w-1/2'>
-                                    <div class='mb-5'>
-                                        <label
-                                            for='date'
-                                            class='mb-3 block text-base font-medium text-[#07074D]'
-                                        >
-                                            Date
-                                        </label>
-                                        <input
-                                            type='date'
-                                            name='date'
-                                            id='date'
-                                            class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                        />
-                                    </div>
-                                </div>
-                                <div class='w-full px-3 sm:w-1/2'>
-                                    <div class='mb-5'>
-                                        <label
-                                            for='time'
-                                            class='mb-3 block text-base font-medium text-[#07074D]'
-                                        >
-                                            Time
-                                        </label>
-                                        <input
-                                            type='time'
-                                            name='time'
-                                            id='time'
-                                            class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class='mb-5 pt-3'>
-                                <label class='mb-5 block text-base font-semibold text-[#07074D] sm:text-xl'>
-                                    Address Details
+                            <div class='mb-5 flex'>
+                                <label
+                                    for='Confirm Password'
+                                    class=' mt-4 w-1/2 '
+                                >
+                                    {t("confirm password")}
                                 </label>
-                                <div class='-mx-3 flex flex-wrap'>
-                                    <div class='w-full px-3 sm:w-1/2'>
-                                        <div class='mb-5'>
-                                            <input
-                                                type='text'
-                                                name='area'
-                                                id='area'
-                                                placeholder='Enter area'
-                                                class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                            />
-                                        </div>
-                                    </div>
-                                    <div class='w-full px-3 sm:w-1/2'>
-                                        <div class='mb-5'>
-                                            <input
-                                                type='text'
-                                                name='city'
-                                                id='city'
-                                                placeholder='Enter city'
-                                                class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                            />
-                                        </div>
-                                    </div>
-                                    <div class='w-full px-3 sm:w-1/2'>
-                                        <div class='mb-5'>
-                                            <input
-                                                type='text'
-                                                name='state'
-                                                id='state'
-                                                placeholder='Enter state'
-                                                class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                            />
-                                        </div>
-                                    </div>
-                                    <div class='w-full px-3 sm:w-1/2'>
-                                        <div class='mb-5'>
-                                            <input
-                                                type='text'
-                                                name='post-code'
-                                                id='post-code'
-                                                placeholder='Post Code'
-                                                class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
+                                <input
+                                    type='password'
+                                    name='Confirm Password'
+                                    class='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 outline-none '
+                                />
                             </div>
-
-                            <div>
-                                <button class='hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none'>
-                                    Book Appointment
+                            <div className='flex gap-4'>
+                                <button class='w-full bg-Accent hover:bg-Primary rounded-md h-12'>
+                                    {t("save changes")}
                                 </button>
+                                <button class='w-full bg-Accent hover:bg-Primary rounded-md h-12'>
+                                    {t("delete account")}
+                                </button>
+                                <button class='w-full bg-Accent hover:bg-Primary rounded-md h-12'>
+                                    {t("cancel")}
+                                </button>
+                            </div>
+                            <h2 className=' my-5 block text-4xl font-semibold '>
+                                {t("payment section")}
+                            </h2>
+                            <div className='flex gap-4'>
+                                <div class='mb-5 '>
+                                    <label
+                                        for='SHOW CARDS'
+                                        class=' mt-4 w-1/2 '
+                                    >
+                                        {t("cards added")}
+                                    </label>
+                                    <button
+                                        name='SHOW CARDS'
+                                        class='w-full bg-Accent hover:bg-Primary rounded-md h-12'
+                                    >
+                                        {t("show cards")}
+                                    </button>
+                                </div>
+                                <div class='mb-5 '>
+                                    <label
+                                        for='BUY TICKETS'
+                                        class=' mt-4 w-1/2 '
+                                    >
+                                        {t("tickets remaining")}
+                                    </label>
+                                    <button
+                                        name='BUY TICKETS'
+                                        class='w-full bg-Accent hover:bg-Primary rounded-md h-12'
+                                    >
+                                        {t("buy tickets")}
+                                    </button>
+                                </div>
                             </div>
                         </form>
                     </div>
