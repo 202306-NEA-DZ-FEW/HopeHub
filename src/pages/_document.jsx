21086ff2eeff5dcf3bdd5cc519af26b76f5e3601
abdelghani bahri <bahri.ghanou@gmail.com>
2023-10-27 01:09:01 +0100

    refactor: remove curly brace
    Create Therapist account #35

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -17,7 +17,6 @@ class MyDocument extends Document {
                         type='font/woff2'
                         crossOrigin='anonymous'
                     />
-
                     <link
                         rel='preconnect'
                         href='https://fonts.googleapis.com'
@@ -30,8 +29,8 @@ class MyDocument extends Document {
                     <link
                         href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;0,800;1,300&display=swap'
                         rel='stylesheet'
-                        
-                    /> */}
+                    />{" "}
+                    */
                     <link
                         href='https://fonts.googleapis.com/css2?family=Aclonica&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
                         rel='stylesheet'
