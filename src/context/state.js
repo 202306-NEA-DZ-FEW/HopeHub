b490e768a0ef5f0aee74bfdd8a3a2d27e7f2ed14
Brahim HM <brahimhm98@gmail.com>
2023-10-27 19:03:21 +0100

    feat(careers page): create the careers page
    #34

--- src/context/state.js
+++ src/context/state.js
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
 import { useTheme } from "next-themes";
+import { createContext, useContext, useState } from "react";
 
 const AppContext = createContext();
 
