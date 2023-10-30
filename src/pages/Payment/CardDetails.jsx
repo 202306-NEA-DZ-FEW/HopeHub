f94a50e8f0b3c6f6a3008c43947387252a07e42b
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-10-30 12:17:36 +0100

    feat(page): create a payment form and a page
    [Payment] #40

---
+++ src/pages/Payment/CardDetails.jsx
@@ -0,0 +1,29 @@
+// pages/CardDetails.js
+
+import React from "react";
+
+import PaymentForm from "@/components/PaymentForm/PaymentForm";
+
+import Layout from "@/layout/Layout";
+
+const CardDetailsPage = () => {
+    return (
+        <Layout>
+            <div className='bg-cover bg-center bg-NeutralWhite flex flex-col'>
+                <h1>Add Card Details</h1>
+                <p>
+                    Please make sure all of the info you enter are the same as
+                    your card registration info.
+                </p>
+
+                <div className='flex flex-col gap-4'>
+                    <div className='flex flex-col gap-2'></div>
+
+                    <PaymentForm />
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default CardDetailsPage;
