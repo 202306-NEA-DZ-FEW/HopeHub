48cd5ed49892957e448b9b6ac17b49e05b93f170
zohir kioukiou <kiouzohir@gmail.com>
2023-11-10 11:02:01 +0100

    fix(requirement page): add a link to the next button
    Added a link to the next button in requirement page to take to therapist account create page
    
    #85

--- src/pages/requirement/index.jsx
+++ src/pages/requirement/index.jsx
@@ -81,11 +81,13 @@ export default function Requirement() {
                         </li>
                     </ul>{" "}
                     {/*Add more list items for requirements here */}
-
-                    <button className=' mt-3 self-end w-28 h-10 rounded-md text-base font-poppins font-regular dark:text-NeutralWhite dark:bg-Dark_Primary dark:group-hover:bg-[#3E4E68]  bg-Accent text-NeutralBlack group-hover:bg-[#879AB8] group-hover:text-NeutralWhite group-hover:scale-105 duration-500'>
+                    <Link
+                        href='/therapist'
+                        className=' mt-3 self-end w-28 h-10 rounded-md text-base font-poppins font-regular dark:text-NeutralWhite dark:bg-Dark_Primary dark:group-hover:bg-[#3E4E68]  bg-Accent text-NeutralBlack group-hover:bg-[#879AB8] group-hover:text-NeutralWhite group-hover:scale-105 duration-500'
+                    >
+                        {" "}
                         {t("Next")}
-                    </button>
-
+                    </Link>
                 </div>
             </div>
         </Layout>
