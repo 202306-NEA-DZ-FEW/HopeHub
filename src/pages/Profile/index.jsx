ff244ff4a5bf95c6647c9f74df1a1b646bf177b9
abdelghani bahri <bahri.ghanou@gmail.com>
2023-11-11 12:52:19 +0100

    refactor(checks): run npm run format to pass the checks
    user role #90

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,30 +1,24 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React from 'react'
+import React from "react";
 
-import TherapistProfile from '@/components/User/updateTherapist'
-import UserProfile from '@/components/User/UserProfile'
+import TherapistProfile from "@/components/User/updateTherapist";
+import UserProfile from "@/components/User/UserProfile";
 
-import { useAppcontext } from '@/context/state'
+import { useAppcontext } from "@/context/state";
 
 function Profile() {
-  const {user}= useAppcontext()
-  console.log('therapist', user)
-  return (
-    <>
-    {
-      user?.isTherapist? <TherapistProfile/> : <UserProfile/>
-    }
-    </>
-  )
+    const { user } = useAppcontext();
+    console.log("therapist", user);
+    return <>{user?.isTherapist ? <TherapistProfile /> : <UserProfile />}</>;
 }
 
-export default Profile
+export default Profile;
 
 export async function getStaticProps({ locale }) {
-  return {
-      props: {
-          ...(await serverSideTranslations(locale, ["common"])),
-          // Will be passed to the page component as props
-      },
-  };
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            // Will be passed to the page component as props
+        },
+    };
 }
